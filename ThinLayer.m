51ee23305205353f87ad8f8db5c8b0c6524f9cc5
gpap-gpap <gpap.gpap@gmail.com>
2018-11-08 11:35:11 +0000

    added discrete hankel transform

--- ThinLayer.m
+++ ThinLayer.m
@@ -6,23 +6,26 @@ BeginPackage["ThinLayer`"]
 
 (* ::Input::Initialization:: *)
 (* Usage Messaging *)
-tlSetLayer::usage="tlSetLayer[index, {c11, c13, c33, c44, \[Rho]}] creates a 'tlLayer[index]' object instance with numerical values for cijs.";
-tlQmatrices::usage="tlQmatices[i, p] calculates the Q matrices corresponding to a pre-defined layer with index i for a horizontal slowness p";
-tlLmatrices::usage="tlLmatices[i, p] calculates the L matrices corresponding to a pre-defined layer with index i for a horizontal slowness p";
-tlLayerReflectionTransmission::usage="tlQLmatices[i, j, p] calculates the reflection and transmission coefficients for an interface at layer \!\(\*
-StyleBox[\"i\",\nFontSlant->\"Italic\"]\) and \!\(\*
-StyleBox[\"j\",\nFontSlant->\"Italic\"]\)\!\(\*
-StyleBox[\" \",\nFontSlant->\"Italic\"]\)for horizontal slowness \!\(\*
-StyleBox[\"p\",\nFontSlant->\"Italic\"]\). Can also be called as tlQLmatices[i, p] in which case the coefficients at the interface between layers \!\(\*
-StyleBox[\"i\",\nFontSlant->\"Italic\"]\), \!\(\*
-StyleBox[\"i\",\nFontSlant->\"Italic\"]\)\!\(\*
-StyleBox[\"+\",\nFontSlant->\"Italic\"]\)\!\(\*
-StyleBox[\"1\",\nFontSlant->\"Italic\"]\) is calculated"; 
-tlLayerPhase::usage="";
-tlReflectivity::usage=""; 
-(* Error Reporting *)
-tlQmatrices::nodef=tlLayerReflectionTransmission::nodef="Layer `1` not defined. Define elastic constants for layer `1` using tlSetLayer";
-tlQmatrices::noint=tlLayerReflectionTransmission::noint="Argument `1` refers to layer label. Integer expected";
+tlSetLayer::usage="tlSetLayer[layerIndex, {c11, c13, c33, c44, \[Rho]}] creates a 'tlLayer[layerIndex]' object instance with assiogned numerical values for cijs.";
+tlQLmatrices::usage="tlQmatices[i, p] calculates the Q matrices corresponding to a pre-defined layer with index i for a horizontal slowness p";
+tlLayerReflectionTransmission::usage="tlLayerReflectionTransmission[i, j, p] calculates transmission and reflection coefficients for an interface between pre-defined layers i and j for horizontal slowness p. It can also be called as tlLayerReflectionTransmission[i, p] in which case an interface between layers i, i+1 is assumed"; 
+tlReflectivity::usage="";
+tlDiscreteHankelTransform::usage="";
+tlDiscreteHankelTransform2::usage="";
+tlVisualiseModel::usage="";
+(* Error Reporting tlSetLayer *)
+tlSetLayer::nonum="Expecting an array of 4 complex and 1 real numeric quantities in arg `2`";
+tlSetLayer::noint="Argument `1` refers to layer label. Positive integer expected";
+tlSetLayer::noagrs="tlSetLayer called with wrong number of arguments. tlSetLayer[layerIndex, {c11, c13, c33, c44, \[Rho]}] expected";
+(* Error Reporting tlQLmatrices *)
+tlQLmatrices::nodef="Layer `1` not defined. Define elastic constants and density for layer `1` using tlSetLayer";
+tlQLmatrices::noint="Argument `1` refers to layer label. Integer expected";
+(* Error Reporting tlLayerReflectionTransmission*)
+tlLayerReflectionTransmission::nodef="Layers `1` and `2` must be defined with tlSetLayer. Define elastic constants and density for layers `1`, `2` using tlSetLayer";
+tlLayerReflectionTransmission::noint="Argument `1` refers to layer label. Integer expected";
+(* Error Reporting tlReflectivity*)
+tlReflectivity::nodef="Layer `1` not defined. Define elastic constants and density for layer `1` using tlSetLayer";
+tlReflectivity::nodef="Argument `1` refers to layer label. Integer expected";
 
 
 (* ::Input::Initialization:: *)
@@ -30,17 +33,20 @@ Begin["Private`"]
 
 
 (* ::Input::Initialization:: *)
-tlSetLayer[index_Integer,{c11_?NumericQ, c13_?NumericQ, c33_?NumericQ, c44_?NumericQ, \[Rho]_Real}]:=
-tlLayer[index] = {c[11]:>N@c11, c[13]:>N@c13,c[33]:>N@c33,c[44]:>N@c44, d:>N@\[Rho] }; 
+tlSetLayer[index_Integer,{c11_?NumericQ, c13_?NumericQ, c33_?NumericQ, c44_?NumericQ, \[Rho]_Real}]/;(Positive[index]):=
+ tlLayer[index]=N@{c11,c13,c33,c44,\[Rho]};
+tlSetLayer[index_Integer,a:({_,_,_,_,_})]/;(Positive[index]):=(Message[tlSetLayer::nonum,index];$Failed);
+tlSetLayer[index_,a:{_,_,_,_,_}]:=(Message[tlSetLayer::noint,index];$Failed);
+tlSetLayer[___]:=(Message[tlSetLayer::noargs];$Failed);
 
 
 (* ::Input::Initialization:: *)
-SetAttributes[tlQmatrices,Listable];
-tlQmatrices[index_Integer,p_]/;ListQ[tlLayer[index]]:=
+SetAttributes[tlQLmatrices,Listable];
+tlQLmatrices[index_Integer,p_]/;ListQ[tlLayer[index]]:=
 Block[{c, d},
-Set@@@tlLayer[index];
+{c[11],c[13],c[33],c[44],d}=tlLayer[index];
 Module[
-{q\[Alpha],q\[Beta],\[Alpha]0,\[Beta]0,\[Eta],\[Delta],\[Sigma]0,S\[Alpha],S\[Beta],R,R1,R2,req1,req2,imq1,imq2},
+{L1,L2,d1,d2,d3,d4,d5,q\[Alpha],q\[Beta],\[Alpha]0,\[Beta]0,\[Eta],\[Delta],\[Sigma]0,S\[Alpha],S\[Beta],R,R1,R2,req1,req2,imq1,imq2},
 \[Alpha]0=Sqrt[c[33]/d];
 \[Beta]0=Sqrt[c[44]/d];
 \[Sigma]0=1-c[44]/c[33];
@@ -59,60 +65,36 @@ imq1=Sign[imq1]imq1;
 imq2=Sign[imq2]imq2;
 q\[Alpha]=Sqrt[req1+I imq1]//Chop;
 q\[Beta]=Sqrt[req2+I imq2]//Chop;
-{q\[Alpha],q\[Beta], S\[Alpha], S\[Beta]}
-]
-];
-tlQmatrices[index_Integer,p_]:=(Message[tlQLmatrices::nodef,index];
-$Failed);
-tlQmatrices[index_,p_]:=(Message[tlQLmatrices::noint,index];
-$Failed);
-
-
-(* ::Input::Initialization:: *)
-tlLmatrices[index_Integer,p_]/;ListQ[tlLayer[index]]:=
-Block[{c, d},
-Set@@@tlLayer[index];
-Module[
-{L1,L2,d1,d2,d3,d4,d5,q\[Alpha],q\[Beta],\[Alpha]0,\[Beta]0,\[Eta],\[Delta],\[Sigma]0,S\[Alpha],S\[Beta]},
-\[Alpha]0=Sqrt[c[33]/d];
-\[Beta]0=Sqrt[c[44]/d];
-\[Sigma]0=1-c[44]/c[33];
-\[Delta]=(c[13]-c[33]+2c[44])/c[33];
-\[Eta]=(c[11] c[33]-(c[13]+2c[44])^2)/(2c[33]^2);
-{q\[Alpha],q\[Beta],S\[Alpha],S\[Beta]}=tlQmatrices[index,p];
-
 d2=Sqrt[(\[Sigma]0+\[Delta])/(\[Sigma]0+S\[Alpha])];
 d3=2\[Beta]0^2 (\[Sigma]0+1/2 (S\[Alpha]+\[Delta]))/(\[Sigma]0+\[Delta]);
 d4=Sqrt[(\[Sigma]0-p^2 \[Beta]0^2 (\[Sigma]0+S\[Beta]))/((1-p^2 \[Beta]0^2 (1+S\[Beta]))(\[Sigma]0+\[Delta]))];
 d5=(\[Sigma]0-2p^2 \[Beta]0^2 (\[Sigma]0+1/2 (S\[Beta]+\[Delta])))/(\[Sigma]0+\[Delta]);
 d1=1/Sqrt[p^2 d3+d5];
-
 L1=d1{
 {d2 Sqrt[q\[Alpha]/d],1/d4 p/Sqrt[d q\[Beta]]},
 {d3 d2 p Sqrt[d q\[Alpha]],-d5/d4 Sqrt[d/q\[Beta]]}
 }//Chop;
-
 L2=d1{
 {d5/d2 Sqrt[d/q\[Alpha]],d3 d4 p Sqrt[d q\[Beta]]},
 {1/d2 p/Sqrt[d q\[Alpha]],-d4 Sqrt[q\[Beta]/d]}
 }//Chop;
-{L1,L2}
+Developer`ToPackedArray/@{{q\[Alpha],q\[Beta]},L1,L2}
 ]
 ];
-tlLmatrices[index_Integer,p_]:=(Message[tlQLmatrices::nodef,index];
+tlQLmatrices[index_Integer,p_]:=(Message[tlQLmatrices::nodef,index];
 $Failed);
-tlLmatrices[index_,p_]:=(Message[tlQLmatrices::noint,index];
+tlQLmatrices[index_,p_]:=(Message[tlQLmatrices::noint,index];
 $Failed);
 
 
 (* ::Input::Initialization:: *)
 SetAttributes[tlLayerReflectionTransmission,Listable];tlLayerReflectionTransmission[index1_Integer,index2_Integer,p_]/;(ListQ[tlLayer[index1]]&&ListQ[tlLayer[index2]]):=
 Module[{L1t, L2t, L1b, L2b, C, D, CpD},
-{L1t, L2t, L1b, L2b}=tlLmatrices[index1,p]~Join~tlLmatrices[index2,p];
+{L1t, L2t, L1b, L2b}=tlQLmatrices[index1,p][[2;;]]~Join~tlQLmatrices[index2,p][[2;;]];
 C=Transpose[L2b].L1t;
 D=Transpose[L1b].L2t;
 CpD=Transpose@Inverse[C+D];
-{2CpD, Transpose[C-D].CpD}//Chop
+{2CpD, Transpose[C-D].CpD}
 ];
 tlLayerReflectionTransmission[index_Integer,p_]:=tlLayerReflectionTransmission[index, index+1,p];
 tlLayerReflectionTransmission[index_Integer,index2_Integer,p_]:=(Message[tlLayerReflectionTransmission::nodef,index];
@@ -122,29 +104,49 @@ $Failed);
 
 
 (* ::Input::Initialization:: *)
-tlLayerPhase[indexSet:{_Integer..},thicknessSet:{_?Positive..}, p_, \[Omega]_]/;(Length@thicknessSet==Length@indexSet):=
-Exp[I \[Omega] thicknessSet tlQmatrices[indexSet,p][[;;,;;2]]]//Chop;
-
-
-(* ::Input::Initialization:: *)
-tlReflectivity[indexSet:{_Integer..},thicknessSet:{_?Positive..}, p_, \[Omega]_]/;((Length@thicknessSet==Length@indexSet)&&(indexSet==Range[indexSet[[1]],indexSet[[1]]+Length[indexSet]-1])):=
-Module[{f, foldList},
-foldList=Reverse@Transpose@{tlLayerReflectionTransmission[Most@indexSet, p],tlLayerPhase[Most@indexSet, Most@thicknessSet,p,\[Omega]]};
-f=(#4.(#3+Transpose[#2].#1.Inverse[IdentityMatrix[2]+#3.#1].#2).#4)&;
-Fold[f[#1,#2[[1,1]],#2[[1,2]],DiagonalMatrix@#2[[2]]]&,{{0,0},{0,0}},foldList]//Chop
-];
 tlReflectivity[indexSet:{_Integer..},thicknessSet:{_?Positive..}, p_, \[Omega]_]/;(Length@thicknessSet==Length@indexSet):=
-Module[{f, foldList},
-foldList=Reverse@Transpose@{tlLayerReflectionTransmission[Most@indexSet, Most@RotateLeft@indexSet, p],tlLayerPhase[Most@indexSet, Most@thicknessSet,p,\[Omega]]};
+Module[{f, foldList, CDmats, TRmats, qL, \[Phi]},
+qL=tlQLmatrices[indexSet,p];
+CDmats=Transpose@MapThread[
+{Transpose[#1[[2]]].#2[[1]],Transpose[#1[[1]]].#2[[2]]}&,
+{RotateLeft@qL[[;;,{2,3}]],qL[[;;,{2,3}]]}];
+TRmats=Transpose@MapThread[Block[{temp=Transpose@Inverse[#1+#2]},{2temp,Transpose[(#1-#2)].temp}]&,CDmats];
+foldList=Reverse@Most@Transpose@{Sequence@@TRmats,DiagonalMatrix/@Exp[I \[Omega] thicknessSet({{0.,0.}}~Join~Rest@qL[[;;,1]])]};
 f=(#4.(#3+Transpose[#2].#1.Inverse[IdentityMatrix[2]+#3.#1].#2).#4)&;
-Fold[f[#1,#2[[1,1]],#2[[1,2]],DiagonalMatrix@#2[[2]]]&,{{0,0},{0,0}},foldList]//Chop
+Fold[f[#1,Sequence@@#2]&,{{0,0},{0,0}},foldList]
 ];
-(*
-tlLayerReflectionTransmission[index_Integer,p_]:=tlLayerReflectionTransmission[index, index+1,p];
-tlLayerReflectionTransmission[index_Integer,index2_Integer,p_]:=(Message[tlLayerReflectionTransmission::nodef,index];
-$Failed);
-tlLayerReflectionTransmission[index_,index_,p_]:=(Message[tlLayerReflectionTransmission::noint,index];
-$Failed);*)
+
+
+(* ::Input::Initialization:: *)
+tlDiscreteHankelTransform[f1_?VectorQ,p_,rmax_]:=Module[{Np,a,aNp1,rv,uv,res,umax,T,J,F1,F2},
+Np=Length@f1;
+a=Developer`ToPackedArray@Table[N[BesselJZero[p,n]],{n,1,Np}];
+aNp1=BesselJZero[p,Np+1];
+umax=aNp1/(2. Pi rmax);
+rv=a/(2. Pi umax);
+uv=a/(2. Pi rmax);
+J=Abs[BesselJ[p+1,a]];
+T=BesselJ[p,TensorProduct[a,a]/(2. Pi rmax umax)]/(TensorProduct[J,J] Pi rmax umax);
+F1=(f1 rmax)/J;
+F2=T.F1;
+Transpose[{uv,J/umax F2}]
+];
+
+
+(* ::Input::Initialization:: *)
+tlDiscreteHankelTransform2[f1_?VectorQ,p_,rmax_]:=Module[{Np,a,aNp1,rv,uv,res,umax,T,J,F1,F2},
+Np=Length@f1;
+a=Developer`ToPackedArray@Table[N[BesselJZero[p,n]],{n,1,Np}];
+aNp1=BesselJZero[p,Np+1];
+umax=aNp1/(2. Pi rmax);
+rv=a/(2. Pi umax);
+uv=a/(2. Pi rmax);
+J=Developer`ToPackedArray@Abs@Table[BesselJ[p+1,s],{s,a}];
+T=Developer`ToPackedArray@(Table[BesselJ[p,s/(2. Pi rmax umax)],{s,TensorProduct[a,a]}]/(TensorProduct[J,J] Pi rmax umax));
+F1=(f1 rmax)/J;
+F2=Developer`ToPackedArray[T.F1];
+Transpose[{uv+0.I,J/umax F2}]
+];
 
 
 (* ::Input::Initialization:: *)
